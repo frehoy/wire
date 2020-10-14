b3d4f11ae8f4d2541f8de906ca21ae9eb8d705f0
Frederick Hoyles <frederick.hoyles@gmail.com>
2020-10-14 22:34:07 +0200

    Updated to new article format, added lxml parser to deps

--- wire.py
+++ wire.py
@@ -1,18 +1,32 @@
+""" Wire
+
+A simple terminal-printing reader for The Wire
+
+"""
+
 import datetime
 import sys
 import time
 import textwrap
+import re
 
 import requests
-from bs4 import BeautifulSoup, NavigableString, Tag
+from bs4 import BeautifulSoup
 
 
 TTYPE_DELAY = 0.03
 FETCH_DELAY = 60
 
-URL_BASE = "http://www.reuters.com"
+URL_BASE = "https://www.reuters.com"
 URL_WIRE = URL_BASE + "/assets/jsonWireNews"
 
+FILTER_EXPRESSIONS = [
+    re.compile(pattern)
+    for pattern in [
+        "Our Standards: The Thomson Reuters Trust Principles.",
+        r"\d* Min Read",
+    ]
+]
 
 
 def ttype_print(text, delay=TTYPE_DELAY, linewidth=80):
@@ -28,63 +42,70 @@ def ttype_print(text, delay=TTYPE_DELAY, linewidth=80):
     sys.stdout.write("\n")
 
 
-
 def get_keywords(soup):
-    kws = soup.find("meta", {"name": "keywords"})['content'].split(",")
+    kws = soup.find("meta", {"name": "keywords"})["content"].split(",")
     kws_separated = ", ".join(kws)
     keywords = f"Keywords: {kws_separated}"
     return keywords
 
 
 def get_paragraphs(soup):
-    try:
-        article_body = soup.find("div", {"class": "StandardArticleBody_body"})
-        paragraphs = article_body.find_all("p")
-        paragraph_texts = [p.get_text() for p in paragraphs]
-    except:
-        paragraph_texts = ""
+    def clean_paragraphs(paragraph_texts):
+        cleaned_paragraph_texts = []
+        for pt in paragraph_texts:
+            if not any([exp.match(pt) for exp in FILTER_EXPRESSIONS]):
+                cleaned_paragraph_texts.append(pt)
 
-    return paragraph_texts
+        return cleaned_paragraph_texts
+
+    article_body = soup.find("div", {"class": "ArticleBodyWrapper"})
+    paragraphs = article_body.find_all("p")
+    paragraph_texts = [p.get_text() for p in paragraphs]
+
+    return clean_paragraphs(paragraph_texts)
 
 
 def get_datetime(headline):
-    ms = int(headline['dateMillis'])
-    dt = str(datetime.datetime.utcfromtimestamp(ms/1000.0))
+    ms = int(headline["dateMillis"])
+    dt = str(datetime.datetime.utcfromtimestamp(ms / 1000.0))
     return dt
 
 
 def get_article(headline):
-    url = URL_BASE + headline['url']
+    url = URL_BASE + headline["url"]
 
     article_raw = requests.get(url)
     soup = BeautifulSoup(article_raw.content, "lxml")
 
     article = {
-        'id': headline['id'],
-        'paragraphs': get_paragraphs(soup),
-        'headline': headline['headline'],
-        'keywords': get_keywords(soup),
-        'datetime': get_datetime(headline),
-        'url': url
+        "id": headline["id"],
+        "paragraphs": get_paragraphs(soup),
+        "headline": headline["headline"],
+        "keywords": get_keywords(soup),
+        "datetime": get_datetime(headline),
+        "url": url,
     }
 
     return article
 
+
 def get_headlines():
     wire_raw = requests.get(URL_WIRE)
-    headlines = wire_raw.json()['headlines']
+    headlines = wire_raw.json()["headlines"]
     return headlines
 
+
 def print_articles(articles):
-    divider = "#"*80
-    for article in sorted(articles, key=lambda a: a['datetime']):
-        ttype_print(article['datetime'])
-        ttype_print(article['headline'])
-        for paragraph in article['paragraphs']:
+    divider = "#" * 80
+    for article in sorted(articles, key=lambda a: a["datetime"]):
+        ttype_print(article["datetime"])
+        ttype_print(article["headline"])
+        for paragraph in article["paragraphs"]:
             ttype_print(paragraph)
-        ttype_print(article['keywords'])
-        ttype_print(article['url'])
+        ttype_print(article["keywords"])
+        ttype_print(article["url"])
         ttype_print(divider)
+
 
 def main():
 
@@ -93,9 +114,9 @@ def main():
         headlines = get_headlines()
         articles = []
         for headline in headlines:
-            if not headline['id'] in finished_ids:
+            if not headline["id"] in finished_ids:
                 article = get_article(headline)
-                finished_ids.append(headline['id'])
+                finished_ids.append(headline["id"])
                 articles.append(article)
 
         print_articles(articles)
